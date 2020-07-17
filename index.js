fff45574060909cf74566e8423f6892f87c37757
Fady Gouda <59597320+fadygouda@users.noreply.github.com>
2020-07-16 20:06:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // code away!
 const server = require('./server.js');
-
-const PORT = 3000;
-
-server.listen(3000, () => {
-    console.log(`Server listening on port ${PORT}`);
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 })
